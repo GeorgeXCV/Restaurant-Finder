d1fa72dbef78beb83095de9d02dc66a581f564a1
George <georgeashton95@gmail.com>
2019-01-05 14:25:51 +0000

    Networking

---
+++ AppDelegate.swift
@@ -0,0 +1,111 @@
+//
+//  AppDelegate.swift
+//  FindRestaurants
+//
+//  Created by George on 20/12/2018.
+//  Copyright © 2018 George. All rights reserved.
+//
+
+import UIKit
+import Moya
+import CoreLocation
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    let window =  UIWindow()
+    let locationService = LocationService()
+    let storyboard = UIStoryboard(name: "Main", bundle: nil)
+    let service = MoyaProvider<YelpService.BusinessesProvider>()
+    let jsonDecoder = JSONDecoder()
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+      
+//        service.request(.details(id: "WavvLdfdP6g8aZTtbBQHTw")) { (result) in
+//            switch result {
+//            case .success(let response):
+//                let details = try? self.jsonDecoder.decode(Details.self, from: response.data)
+//                print("Details: \n\n \(details)")
+//            case .failure(let error):
+//                print("Failed to get details \(error)")
+//            }
+//        }
+        
+        jsonDecoder.keyDecodingStrategy = .convertFromSnakeCase
+        
+        locationService.didChangeStatus = { [weak self] success in
+            if success {
+                self?.locationService.getLocation()
+            }
+        }
+        
+        locationService.newLocation = { [weak self] result in
+            switch result {
+            case .success(let location):
+                self?.loadBusinesses(with: location.coordinate)
+            case .failure(let error):
+                assertionFailure("Error getting the users location \(error)")
+            }
+        }
+        
+        switch locationService.status {
+        case .notDetermined, .denied, .restricted:
+            let locationViewController = storyboard.instantiateViewController(withIdentifier: "LocationViewController") as? LocationViewController
+            locationViewController?.delegate = self
+            window.rootViewController = locationViewController
+        default:
+            let nav = storyboard
+                .instantiateViewController(withIdentifier: "RestaurantNavigationController") as? UINavigationController
+            window.rootViewController = nav
+            locationService.getLocation()
+             (nav?.topViewController as? RestaurantTableViewController)?.delegete = self
+        }
+        window.makeKeyAndVisible()
+        
+        return true
+    }
+    
+    private func loadDetais(withId id: String) {
+        service.request(.details(id: id)) { [weak self] (result) in
+            switch result {
+            case .success(let response):
+                guard let strongSelf = self else { return }
+                let details = try? strongSelf.jsonDecoder.decode(Details.self, from: response.data)
+                print("Details: \n\n \(details)")
+            case .failure(let error):
+                print("Failed to get details \(error)")
+            }
+        }
+    }
+    
+
+    
+    private func loadBusinesses(with coordinate: CLLocationCoordinate2D) {
+        service.request(.search(lat: coordinate.latitude, long: coordinate.longitude)) { [weak self] (result) in
+            switch result {
+            case .success(let response):
+                guard let strongSelf = self else { return }
+                let root = try? strongSelf.jsonDecoder.decode(Root.self, from: response.data)
+                let viewModels = root?.businesses
+                    .compactMap(RestaurantListViewModel.init)
+                    .sorted(by: { $0.distance < $1.distance })
+                if let nav = strongSelf.window.rootViewController as? UINavigationController,
+                    let restaurantListViewController = nav.topViewController as? RestaurantTableViewController {
+                    restaurantListViewController.viewModels = viewModels ?? []
+                }
+            case .failure(let error):
+                print("Error: \(error)")
+            }
+        }
+    }
+}
+
+extension AppDelegate: LocationActions, ListActions {
+    func didTapAllow() {
+        locationService.requestLocationAuthorization()
+    }
+    func didTapCell(_ viewModel: RestaurantListViewModel) {
+        loadDetais(withId: viewModel.id)
+    }
+}
